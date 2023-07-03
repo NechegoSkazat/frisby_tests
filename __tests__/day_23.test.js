fc75a95f0adc7491beb7bfa6bbc3f00ec4100e14
NechegoSkazat <kotleta88@gmail.com>
2023-07-03 22:29:34 +0200

    Slice out last string of data

--- __tests__/day_23.test.js
+++ __tests__/day_23.test.js
@@ -10,7 +10,7 @@ var data = fs.readFileSync('__tests__/geoMap.csv')
 
 
 describe("Day_23: Data files", () => {
-    it.each(data.slice(1))("GET return status 200", async (region, boba) => {
+    it.each(data.slice(1, -1))("GET return status 200", async (region, boba) => {
           console.log(region, boba);
           await frisby
             .get(`${BASE_URL}?${region}=${boba}`)
